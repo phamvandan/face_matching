0d156e4832352d73ea4105fc673087de1088896d
dan <phamvandan.cntt2@gmail.com>
2020-08-14 16:06:39 +0700

    initial

---
+++ detect_folder.py
@@ -0,0 +1,121 @@
+'''
+Uses weights and models implementation' from
+https://github.com/deepinsight/insightface
+'''
+
+import imutils
+import os
+import cv2
+from os import path, listdir, makedirs
+import argparse
+import numpy as np
+import glob
+from multiprocessing import Pool
+from os import path, makedirs
+from sklearn.metrics.pairwise import cosine_similarity
+import sys
+sys.path.insert(0, '../insightface/deploy/')
+import face_model
+
+
+def rotate(mat, angle):
+    if angle==0:
+        return mat
+    height, width = mat.shape[:2]
+    image_center = (width/2, height/2)
+    rotation_mat = cv2.getRotationMatrix2D(image_center, angle, 1.)
+    abs_cos = abs(rotation_mat[0,0]) 
+    abs_sin = abs(rotation_mat[0,1])
+    bound_w = int(height * abs_sin + width * abs_cos)
+    bound_h = int(height * abs_cos + width * abs_sin)
+    rotation_mat[0, 2] += bound_w/2 - image_center[0]
+    rotation_mat[1, 2] += bound_h/2 - image_center[1]
+    rotated_mat = cv2.warpAffine(mat, rotation_mat, (bound_w, bound_h))
+    return rotated_mat
+
+def read_image_from_folder(folder_name):
+    print("reading...")
+    images = []
+    for path, subdirs, files in os.walk(folder_name):
+        for name in files:
+            filename = os.path.join(path, name)
+            img = cv2.imread(filename)
+            images.append(img)
+    print("end read")
+    return images
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Extract features with CNN')
+    parser.add_argument('--folder', '-f', help='image folder')
+    parser.add_argument('--savefolder', '-sf', help='image folder')
+    parser.add_argument('--thresh', '-th', default=0.5, help='threshold')
+    # ArcFace params
+    parser.add_argument('--image-size', default='112,112', help='')
+    parser.add_argument('--model', help='path to model.',
+                        default='../../insightface/models/model-r100-ii/model,0')
+    parser.add_argument('--ga-model', default='', help='path to load model.')
+    parser.add_argument('--gender_model', default='',
+                        help='path to load model.')
+    parser.add_argument('--gpu', default=0, type=int, help='gpu id')
+    parser.add_argument('--det', default=1, type=int,
+                        help='mtcnn: 1 means using R+O, 0 means detect from begining')
+    parser.add_argument('--flip', default=0, type=int,
+                        help='whether do lr flip aug')
+    parser.add_argument('--threshold', default=1.24,
+                        type=float, help='ver dist threshold')
+
+    args = parser.parse_args()
+    args.gpu = -1
+    args.det = 0
+    args.model = ""
+    img_heights = [1000, 800, 600, 400, 250]
+    model = face_model.FaceModel(args)
+    print("Thresh:", float(args.thresh))
+    first = True
+    images = read_image_from_folder(args.folder)
+    if len(images) > 0:
+        savepath = args.savefolder
+        cp_path = savepath
+        if not os.path.exists(savepath):
+            os.mkdir(savepath)
+        f = open(os.path.join(savepath, "result.txt"), "w+")
+        f.write("Total images: " + str(len(images))+"\n")
+        t = 0
+        for img_height in img_heights:
+            savepath = os.path.join(cp_path,str(img_height))
+            if not os.path.exists(savepath):
+                os.mkdir(savepath)
+            f.write("img_height: " + str(img_height)+"\n")
+            count = 0
+            count_rotate = 0
+            total_time = 0
+            for index, img in enumerate(images):
+                if img is None:
+                    continue
+                if img.shape[0] >= img_height:
+                    img = imutils.resize(img, height=img_height)
+                for i in range(4):
+                    img = rotate(img, 90*i)
+                    if i > 0:
+                        count_rotate = count_rotate + 1
+                    _, cropped, t = model.get_input(img)
+                    total_time = total_time + t
+                    if cropped is not None:
+                        break
+                if cropped is None:
+                    count = count + 1
+                    cv2.imwrite(os.path.join(
+                        savepath, str(count)+str(img_height)+".jpg"), images[index])
+                    continue
+            f.write("Can not detect: "+str(count)+"\n")
+            f.write("Rotate: "+str(count_rotate)+"\n")
+            f.write("Total times:" + str(total_time)+"\n")
+            f.write("Avg times:" + str(total_time/len(images)))
+            f.write("--------------------------------------\n")
+                # cv2.imshow("cropped", cropped)
+                # key = cv2.waitKey(1)
+                # if key == ord('q'):
+                #     break
+        print("STOP")
+    
